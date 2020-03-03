f3f9020f9064ef2206770a9c377ca2a2acf1dcb0
Md Ashraful Alam <ashrafulnaim8@gmail.com>
2020-03-02 23:15:26 -0500

    update

--- publish/js/displayPost.js
+++ publish/js/displayPost.js
@@ -15,9 +15,18 @@ function createPost(postData, userInfo, postId) {
 	const text = js.createEl('div', 'post-text', postData.text);
 	post.appendChild(text);
 
-	// post info 
+	// post info
+	const postInfo = js.createEl('div', 'post-info');
+	post. appendChild(postInfo);
 
 	// author userInfo.displayName
-	// date js.formatDate(postData.date) 
+	const author = js.createEl('div', 'post-author', 'userInfo.displayName');
+	post. appendChild(author);
+
+	//post date
+	const date = js.createEl('div', 'post-date', js.formatDate (postDate.date);
+	postInfo. appendChild(date);
+
+	cosole.log(date);
 	
 }
