bd5c0bcd2e8d00de58a292ac25ebe8beac7794f2
Md Ashraful Alam <ashrafulnaim8@gmail.com>
2020-03-26 23:04:18 -0400

    update

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -4,10 +4,10 @@ fb.getUserProfile(profileUID);
 
 const profileName = js.getEl("profile-display-name");
 const profileGender = js.getEl("profile-gender");
-const profileZipCode = js.getEl("profile-zipCode");
+const profilezipcode = js.getEl("profile-zipCode");
 const profileBio = js.getEl("profile-bio");
 const profileLocation = js.getEl("profile-location");
-const profileContactNumber = js.getEl("profile-contactNumber");
+const profilecontactnumber = js.getEl("profile-contactNumber");
 const profileButton = js.getEl("update-profile");
 
 function displayProfile(userName, userInfo) {
@@ -44,10 +44,10 @@ profileButton.onclick = function() {
 	// helper: user id, field name 
 	fb.updateProfile(profileUID, 'displayName', profileName.value);
 	fb.updateProfile(profileUID, 'gender', profileBio.value);
-	fb.updateProfile(profileUID, 'zipCode', profileBio.value);
+	fb.updateProfile(profileUID, 'zipcode', profileBio.value);
 	fb.updateProfile(profileUID, 'bio', profileBio.value);
 	fb.updateProfile(profileUID, 'location', profileBio.value);
-	fb.updateProfile(profileUID, 'contactNumber', profileBio.value);
+	fb.updateProfile(profileUID, 'contactnumber', profileBio.value);
 };
 
 
