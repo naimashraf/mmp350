32aa228a2db5bc4fd8e8786ecd732c87b504b60f
Md Ashraful Alam <ashrafulnaim8@gmail.com>
2020-04-06 20:31:58 -0400

    grid

---
+++ layout/js/publish.js
@@ -0,0 +1,17 @@
+
+const publishText = js.getEl('publish-text');
+const publishButton = js.getEl('publish-button');
+
+publishText.addEventListener('keyup', function(event) {
+	if (event.which == 13) {
+		publishPost();
+	}
+});
+
+publishButton.addEventListener('click', publishPost);
+
+function publishPost() {
+	const uid = fb.getUID();
+	fb.publishPost(uid, publishText.value);
+	publishText.value = ""; // reset textarea
+}
